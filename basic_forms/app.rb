d33329abd1136f5e691830e97350d7841273bda7
Kyle Shike <kyleshike@gmail.com>
2013-10-23 17:55:02 -0400

    finish basic-forms lab

--- basic_forms/app.rb
+++ basic_forms/app.rb
@@ -7,9 +7,25 @@ module FormsLab
       erb :'songs/index'
     end
 
-    get '/songs/:id' do
+    get '/songs/:id' do  
       @song = Song[params[:id]]
       erb :'songs/show'
+    end
+
+    get '/songs/:id/edit' do
+      @song = Song[params[:id]]
+      # binding.pry
+      erb :'songs/edit'
+    end
+
+    post '/songs/:id/update' do
+      @song = Song[params[:id]]
+      @song.title = params[:title]
+      @song.artist = params[:artist]
+      @song.genre = params[:genre]
+      @song.length = params[:length]
+      @song.save
+      redirect '/'
     end
   end
 end
